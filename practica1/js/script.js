ebe94354dcb5325ab37560898c4e9c225f2b6925
Jocawl <jony0685@gmail.com>
2019-03-24 19:23:23 +0100

    correcion errores defensa 1 y comienzo p2

---
+++ practica1/js/script.js
@@ -0,0 +1,13 @@
+function mostrar() {
+    //document.getElementById("comment").style.width = "300px";
+    //document.getElementById("comment").style.marginLeft = "300px";
+    document.getElementById("abrir").style.display = "none";
+    document.getElementById("cerrar").style.display = "inline";
+}
+/*
+function ocultar() {
+    document.getElementById("comment").style.width = "0";
+    document.getElementById("comment").style.marginLeft = "0";
+    document.getElementById("abrir").style.display = "inline";
+    document.getElementById("cerrar").style.display = "none";
+}*/
