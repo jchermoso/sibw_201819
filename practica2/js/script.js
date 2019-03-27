c8cc2923c97e4d853b5da396ac5a6af2e25eee10
Jocawl <jony0685@gmail.com>
2019-03-27 21:12:55 +0100

    funcion que filtra las palabras malsonantes

--- practica2/js/script.js
+++ practica2/js/script.js
@@ -1,3 +1,28 @@
+/** Buscar y reemplazar palabras malsonantes **/
+var textobusq = "";
+var textorempl = "";
+var textolisto = "";
+var palabras = ["puta", "gilipollas", "maricon", "polla"];
+
+//Hasta que no se cargue la página no se ejecuta la función
+window.onload = function () {
+    document.getElementById("comentario").addEventListener("keyup", listenEvent);
+}
+
+function listenEvent(){
+    var comment = document.getElementById('comentario').value;
+    var words = comment.split(" ");
+
+    for(id in words){
+        var position = palabras.indexOf(words[id]);
+        if(palabras.indexOf(words[id]) != -1){
+            comment = comment.replace(palabras[position], ''.padStart(palabras[position].length, "*"))
+        }
+    }
+
+    document.getElementById('comentario').value = comment;
+}
+
 function mostrar() {
     document.getElementById("comment").style.display = "block";
     document.getElementById("abrir").style.display = "none";
@@ -8,53 +33,4 @@ function ocultar() {
     document.getElementById("comment").style.display = "none";
     document.getElementById("abrir").style.display = "inline";
     document.getElementById("cerrar").style.display = "none";
-}
-
-/** Buscar y reemplazar palabras malsonantes **/
-var textobusq = "";
-var textorempl = "";
-var textolisto = "";
-var palabras = ["puta", "gilipollas", "maricon", "polla"];
-
-//Recoge el texto que se desea buscar para su posterior remplazo
-function find(){
-    textobusq = document.getElementById("text0").value;
-    return  textobusq;
-}
-
-function STRTemp(textremp){
-    //Recoge el texto con el que se desea remplazar
-    //textorempl = document.getElementById("text1").value;
-    textorempl = "*";
-    
-    textolisto = textremp.replace(new RegExp(find(),"g") ,textorempl);
-  
-    
-    return  textolisto;
-   
-}
-
-function filtro(){
-    var text = document.getElementById("comentario").value;
-    if (textremp.search(find()) < 0) {
-        alert("No hay resultados");
-    } else {
-                document.getElementById("tremp").value = STRTemp(textremp);
-            }
-}
-function replace(){
-  //Comprueba que hay texto en las casillas
-    if (find().length == 0) {
-        alert("No hay palabra para buscar");
-    } else {
-        
-        var textremp = document.getElementById("tremp").value;
-        
-            if (textremp.search(find()) < 0) {
-                alert("No hay resultados");
-            } else {
-                document.getElementById("tremp").value = STRTemp(textremp);
-            }
-    }
-    
 }
