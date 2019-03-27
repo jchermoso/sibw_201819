78fcb95aeb7ee3e57b12e03fa2e2315c715241bb
Jocawl <jony0685@gmail.com>
2019-03-27 23:46:35 +0100

    cambios p2

--- practica2/js/script.js
+++ practica2/js/script.js
@@ -7,6 +7,8 @@ var palabras = ["puta", "gilipollas", "maricon", "polla"];
 //Hasta que no se cargue la página no se ejecuta la función
 window.onload = function () {
     document.getElementById("comentario").addEventListener("keyup", listenEvent);
+    document.getElementById("comentario").addEventListener("focus", validate);
+    document.getElementById("form").addEventListener("submit", submit);
 }
 
 function listenEvent(){
@@ -23,14 +25,35 @@ function listenEvent(){
     document.getElementById('comentario').value = comment;
 }
 
+function submit(event){
+    event.preventDefault();
+    document.getElementById('copy_values').style.display = "block";
+    document.getElementById('copy_name').textContent  = document.getElementById('nombre').value;
+    document.getElementById('copy_email').textContent  = document.getElementById('email').value;
+    document.getElementById('copy_comment').textContent  = document.getElementById('comentario').value;
+    
+    document.getElementById('form').reset()
+}
+
+function validate(){
+    var txtName = document.getElementById('nombre').value;
+    var txtCorreo = document.getElementById('email').value;
+    
+    if(txtName != null && txtCorreo != null) {
+        document.getElementById("comentario").setAttribute("disabled", true);
+    }else {
+        document.getElementById("comentario").removeAttribute("disabled");
+    }
+}
+
 function mostrar() {
     document.getElementById("comment").style.display = "block";
     document.getElementById("abrir").style.display = "none";
-    document.getElementById("cerrar").style.display = "inline";
+    document.getElementById("cerrar").style.display = "inline-table";
 }
 
 function ocultar() {
     document.getElementById("comment").style.display = "none";
-    document.getElementById("abrir").style.display = "inline";
+    document.getElementById("abrir").style.display = "inline-table";
     document.getElementById("cerrar").style.display = "none";
 }
