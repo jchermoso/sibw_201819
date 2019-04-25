0f96c6876f115806e0840735f77cc1775963761d
jchermoso <32912623+jchermoso@users.noreply.github.com>
2019-04-25 16:01:16 +0200

    Add files via upload

--- practica3/index.php
+++ practica3/index.php
@@ -1,9 +1,17 @@
 <?php 
+
+include("src/db_connect.php");
+
+$mysqli = new Connect("usuario","lechuza","sibw");
+
+var_dump($mysqli);  
+
 require_once 'vendor/autoload.php';
-
-$loader = new \Twig\Loader\FilesystemLoader('templates');
+$loader = new \Twig\Loader\FilesystemLoader('template');
 $twig = new \Twig\Environment( $loader,[]);
+echo $twig->render('pruebaTemplate.html' ,
+    ['nombre' => 'Espinete' ,'edad' => 'Indefinida']);
 
-echo $twig->render('portada.html');
+
 ?>
 
