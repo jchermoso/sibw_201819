8f1dfca0fa2a23795319834b975eb0ed5e654bf5
Jocawl <jony0685@gmail.com>
2019-04-28 20:35:09 +0200

    limpieza

--- practica3/evento.php
+++
@@ -1,5 +0,0 @@
- <html>
-    <?php
-        include("header.php");
-    ?>
- </html>
--- practica3/src/footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-    <p>© 2019 - Jonathan Ord&oacute;ñez Cubero - Juan Carlos Hermoso Quesada</p>
-</footer>
