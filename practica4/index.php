59b1ff22834b772600bd740999a0719a4eed1a3c
Jocawl <jony0685@gmail.com>
2019-05-20 18:44:00 +0200

    actualizo

--- practica4/index.php
+++ practica4/index.php
@@ -29,6 +29,7 @@ switch ($request) {
         $id = $_POST["idEvento"];
         $renderparams["menu"] = $bd->select_menu();
         $renderparams["evento"] = $bd->getEvento($id);
+        $renderparams["eventos"] = $bd->select_evento();
         $renderparams["galeria"] = $bd->select_galeria();
         $renderparams["comentarios"] = $bd->select_comentario($id);
         
@@ -36,6 +37,7 @@ switch ($request) {
         break;
     case '/contacto':
         $renderparams["menu"] = $bd->select_menu();
+        $renderparams["eventos"] = $bd->select_evento();
         
         echo $twig->render('contacto.html', $renderparams);
         break;
@@ -54,6 +56,7 @@ switch ($request) {
         $id = $_POST["idEvento"];
         $renderparams["menu"] = $bd->select_menu();
         $renderparams["evento"] = $bd->getEvento($id);
+        $renderparams["eventos"] = $bd->select_evento();
         $renderparams["galeria"] = $bd->select_galeria();        
         $renderparams["imprimir"] = TRUE;
     
@@ -61,8 +64,9 @@ switch ($request) {
         break;
     case '/login':
         $renderparams["menu"] = $bd->select_menu();
-        echo $twig->render('login.php',$renderparams);
-        
+        $renderparams["eventos"] = $bd->select_evento();
+
+        echo $twig->render('login.html',$renderparams);
         break;
     case '/registro':
         if (isset($_POST["nombre"]) && isset($_POST["email"]) && isset($_POST["pass"])) {
@@ -74,15 +78,15 @@ switch ($request) {
         }
     
         $renderparams["menu"] = $bd->select_menu();
-        $renderparams["usuarios"] = $bd->select_usuarios();
+        $renderparams["eventos"] = $bd->select_evento();
+        //$renderparams["usuarios"] = $bd->select_usuarios();
         
         echo $twig->render('registro.html',$renderparams);
         break;
     
     default:
         $renderparams['error'] = 404;
-        
-        
+
         echo $twig->render('404.html', $renderparams);
         break;
 }
